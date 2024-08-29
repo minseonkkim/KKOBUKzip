75760ab82c0a802a1c979b984e5cc6b53d89fd09
김여준 <kt65346337@gmail.com>
2024-08-29 17:38:56 +0900

    initial commit

---
+++ backend/src/test/java/com/turtlecoin/kkobukzip/KkobukzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtlecoin.kkobukzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkobukzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
