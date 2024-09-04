05c8229e7bf1a7e482ae6e0db6696076c0f673c3
김여준 <kt65346337@gmail.com>
2024-09-04 17:46:53 +0900

    chore: MSA 스켈레톤 코드 작성

---
+++ backend/auction-service/src/test/java/com/turtlecoin/auctionservice/AuctionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtlecoin.auctionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/main-service/src/test/java/com/turtlecoin/mainservice/MainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtlecoin.mainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
