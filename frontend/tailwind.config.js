26e29c5c56df2493161cb57f944ba5e34128ced0
minseonkkim <minsun9856@gmail.com>
2024-09-05 11:33:06 +0900

    chore: 폰트 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'dnf-bitbit': ["DNFBitBitv2"],
+        'KoPubDotum': ["KoPub Dotum"],
+      },
+    },
   },
   plugins: [],
 };
