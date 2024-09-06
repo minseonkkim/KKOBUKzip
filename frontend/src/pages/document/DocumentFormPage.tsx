de4ae13a17494c4505af57991925b317cb19087d
한세훈 <atdark04@gmail.com>
2024-09-06 15:54:44 +0900

    feat: 양수/양도 static한 부분 완성

--- frontend/src/pages/document/DocumentFormPage.tsx
+++ frontend/src/pages/document/DocumentFormPage.tsx
@@ -1,10 +1,18 @@
 import { useCallback, useEffect, useState } from "react";
 // import { Helmet } from "react-helmet-async";
 import BreedDocument from "../../components/document/BreedDocument";
-type TabName = "인공증식" | "양도" | "양수" | "폐사/질병"; // 필요한 탭 이름들을 여기에 추가
+import GrantorDocument from "../../components/document/GrantorDocument";
+import AssigneeDocument from "../../components/document/AssigneeDocument";
+import MyDocumentDataForm from "../../components/document/MyDocumentDataForm";
+import { useLocation } from "react-router-dom";
+type TabName = "인공증식" | "양도" | "양수" | "폐사/질병" | ""; // 필요한 탭 이름들을 여기에 추가
+
+// 각 컴포넌트의 구비서류 부분 정비할것!!!
 
 function DocumentFormPage() {
-  const [activeTab, setActiveTab] = useState<TabName>("인공증식"); // 추후에 동적 할당
+  const [activeTab, setActiveTab] = useState<TabName>(""); // 추후에 동적 할당
+  const [tabNameList, setTabNameList] = useState<TabName[]>([]);
+  const location = useLocation();
 
   // 주소 찾기 스크립트 + 로드
   const loadDaumPostcodeScript = useCallback(() => {
@@ -19,19 +27,28 @@ function DocumentFormPage() {
     loadDaumPostcodeScript();
   }, [loadDaumPostcodeScript]);
 
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/doc-form/transfer-acquisition":
+        setTabNameList(["양도", "양수"]);
+        break;
+      case "/doc-form/breed-death":
+        setTabNameList(["인공증식", "폐사/질병"]);
+        break;
+      // default:
+      // break;
+    }
+  }, []);
+
   return (
     <>
-      {/* <Helmet>
-        <title>문서폼페이지</title>
-      </Helmet> */}
-
-      <div className="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-lg">
+      <div className="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-lg mb-10">
         <h2 className="text-2xl font-bold mb-6 text-center">서류 등록</h2>
 
         {/* 상단 선택 탭 */}
         <div className="mb-6 flex justify-center border-b">
           <div className="flex">
-            {["인공증식", "양도", "양수", "폐사/질병"].map((tab) => (
+            {tabNameList.map((tab) => (
               <button
                 key={tab}
                 className={`px-4 py-2 ${
@@ -49,37 +66,12 @@ function DocumentFormPage() {
         {/* 선택 탭 끝 */}
 
         {/* 신청인 정보 -데이터 연동되면 할당할 것 */}
-        <div className="mb-8">
-          <h3 className="text-xl font-semibold mb-4">신청인 정보</h3>
-          <div className="grid grid-cols-2 gap-x-8 gap-y-4">
-            <div className="flex">
-              <span className="w-1/3 font-medium">성명</span>
-              <span className="w-2/3">김씨피</span>
-            </div>
-            <div className="flex">
-              <span className="w-1/3 font-medium">국적</span>
-              <span className="w-2/3">내국인</span>
-            </div>
-            <div className="flex">
-              <span className="w-1/3 font-medium">전화번호</span>
-              <span className="w-2/3">010-3333-3333</span>
-            </div>
-            <div className="flex">
-              <span className="w-1/3 font-medium">생년월일</span>
-              <span className="w-2/3">2024-01-02</span>
-            </div>
-            <div className="flex">
-              <span className="w-1/3 font-medium">이메일</span>
-              <span className="w-2/3">ssafy@ssafy.com</span>
-            </div>
-            <div className="flex">
-              <span className="w-1/3 font-medium">주소</span>
-              <span className="w-2/3">광주광역시 하남산단6번로 107</span>
-            </div>
-          </div>
-        </div>
+        <MyDocumentDataForm />
         {/* 신청인 정보 끝*/}
+
         {activeTab === "인공증식" && <BreedDocument />}
+        {activeTab === "양도" && <GrantorDocument />}
+        {activeTab === "양수" && <AssigneeDocument />}
       </div>
     </>
   );
