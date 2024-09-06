8496245006d7c8c683b7863e490d01c0a243aebf
서규범 <seo980916@gmail.com>
2024-09-06 13:35:59 +0900

    feat: TurtleToken 컨트랙트 배포 파일 수정

--- blockchain/migrations/1_deploy_turtletoken.js
+++ blockchain/migrations/1_deploy_turtletoken.js
@@ -1,7 +1,12 @@
 const TurtleToken = artifacts.require("TurtleToken");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(TurtleToken, 1000000, 1000); // 두 번째 인자: initialSupply, 세 번째 인자: _exchangeRate
+  // web3.utils.toWei를 사용하여 큰 숫자 처리
+  const initialSupply = web3.utils.toWei("1000000000", "ether"); // 1,000,000,000 TURT (18자리 소수점 사용)
+  const exchangeRate = web3.utils.toWei("3000000", "ether"); // 3,000,000 TURT / 1 ETH
+
+  // TurtleToken 배포
+  await deployer.deploy(TurtleToken, initialSupply, exchangeRate);
   const turtleTokenInstance = await TurtleToken.deployed();
 
   console.log("===========================================================");
