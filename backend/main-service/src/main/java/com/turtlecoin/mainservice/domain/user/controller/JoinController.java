c83fb811e6e25a333fb730dd5d7d06aa3c2816f1
신민경 <tlsalsrud252@naver.com>
2024-09-10 17:37:32 +0900

    feat: 회원가입 controller 작성

--- backend/main-service/src/main/java/com/turtlecoin/mainservice/domain/user/controller/JoinController.java
+++ backend/main-service/src/main/java/com/turtlecoin/mainservice/domain/user/controller/JoinController.java
@@ -9,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@RequestMapping("/api/main")
 @Controller
 @ResponseBody
 public class JoinController {
