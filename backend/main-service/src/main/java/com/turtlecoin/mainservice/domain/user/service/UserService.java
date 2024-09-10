ae965bce5eb4482f6df7bf1c5cf82ddb9952a15e
신민경 <tlsalsrud252@naver.com>
2024-09-10 17:41:06 +0900

    feat: UserService 추가

--- backend/main-service/src/main/java/com/turtlecoin/mainservice/domain/user/service/UserService.java
+++ backend/main-service/src/main/java/com/turtlecoin/mainservice/domain/user/service/UserService.java
@@ -4,7 +4,11 @@ import com.turtlecoin.mainservice.domain.user.dto.UserRequestDto;
 import com.turtlecoin.mainservice.domain.user.entity.Role;
 import com.turtlecoin.mainservice.domain.user.entity.User;
 import com.turtlecoin.mainservice.domain.user.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class UserService {
@@ -14,6 +18,7 @@ public class UserService {
     }
     public User saveUser(UserRequestDto dto) {
         Role role = (dto.getRole() == null) ? Role.ROLE_USER : dto.getRole();
+        String uuid = String.valueOf(new SecretKeySpec(dto.getEmail().getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm()));
         User user = new User(
                 dto.getEmail(),
                 dto.getPassword(),
@@ -23,8 +28,9 @@ public class UserService {
                 dto.getBirth(),
                 dto.getPhonenumber(),
                 dto.getAddress(),
-                dto.getUuid(),
-                role
+                role,
+                dto.getProfileImage(),
+                uuid
         );
         return userRepository.save(user);
     }
