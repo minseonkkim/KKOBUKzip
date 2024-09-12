c031119ac2293f9f1f35ecab0140b0a956300bb8
minseonkkim <minsun9856@gmail.com>
2024-09-13 08:59:47 +0900

    feat: 거래 리스트 페이지 props 분리

--- frontend/src/components/transaction/TransactionTurtle.tsx
+++ frontend/src/components/transaction/TransactionTurtle.tsx
@@ -1,17 +1,30 @@
-import TmpTurtle from "../../assets/tmp_turtle.jpg";
 import TransactionStatusTag from "./TransactionStatusTag";
 
-export default function TransactionTurtle() {
+interface TransactionTurtleProps {
+  scientific_name: string;
+  price: number;
+  transaction_tag: string[];
+  transaction_image: string[];
+  progress: string;
+}
+
+export default function TransactionTurtle({
+  scientific_name,
+  price,
+  transaction_image,
+  transaction_tag,
+  progress,
+}: TransactionTurtleProps) {
   return (
     <>
       <div className="bg-[#F8F9FA] shadow-lg transition-shadow duration-300 w-full max-w-sm h-[300px] rounded-2xl flex flex-col cursor-pointer active:scale-95 relative">
         <div className="absolute top-4 right-4 z-10">
-          <TransactionStatusTag progress={"거래가능"} />
+          <TransactionStatusTag progress={progress} />
         </div>
 
         <div className="overflow-hidden rounded-t-2xl relative">
           <img
-            src={TmpTurtle}
+            src={transaction_image[0]}
             className="w-full h-[200px] object-cover transition-transform duration-300 hover:scale-110"
             draggable="false"
             alt="거북이 이미지"
@@ -19,13 +32,20 @@ export default function TransactionTurtle() {
         </div>
         <div className="px-4 py-1">
           <div className="flex justify-between items-center my-2">
-            <h3 className="text-xl font-semibold">페닐슐라쿠터</h3>
-            <div className="text-[16px] text-gray-700">
-              <span className="bg-[#D5F0DD] text-[#065F46] px-2 py-1 rounded-full">#태그</span>
+            <h3 className="text-xl font-semibold">{scientific_name}</h3>
+            <div className="text-[16px] text-gray-700 flex space-x-2">
+              {transaction_tag.map((tag, index) => (
+                <span
+                  key={index}
+                  className="bg-[#D5F0DD] text-[#065F46] px-2 py-1 rounded-full"
+                >
+                  #{tag}
+                </span>
+              ))}
             </div>
           </div>
           <div className="font-extrabold text-3xl text-[#4B721F] mt-1">
-            3,000,000원
+            {price.toLocaleString("ko-KR")}
           </div>
         </div>
       </div>
--- frontend/src/pages/transaction/TransactionListPage.tsx
+++ frontend/src/pages/transaction/TransactionListPage.tsx
@@ -6,6 +6,12 @@ import { useState } from "react";
 import TransactionTurtle from "../../components/transaction/TransactionTurtle";
 import { IoIosSearch } from "react-icons/io";
 import { IoFilterOutline } from "react-icons/io5";
+import TmpTurtle1 from "../../assets/tmp_turtle.jpg";
+import TmpTurtle2 from "../../assets/tmp_turtle_2.jpg";
+import TmpTurtle3 from "../../assets/tmp_turtle_3.jpg";
+import TmpTurtle4 from "../../assets/tmp_turtle_4.jpg";
+import TmpTurtle5 from "../../assets/tmp_turtle_5.jpg";
+import TmpTurtle6 from "../../assets/tmp_turtle_6.jpg";
 
 type FilterType = 'gender' | 'size' | 'price';
 
@@ -203,11 +209,12 @@ function TransactionListPage() {
         )}
 
         <div className="grid grid-cols-3 gap-4 mb-[30px] mt-[10px]">
-          <TransactionTurtle/>
-          <TransactionTurtle/>
-          <TransactionTurtle/>
-          <TransactionTurtle/>
-          <TransactionTurtle/>
+          <TransactionTurtle scientific_name="지오프리 사이드 넥 터틀" price={300000000} transaction_tag={["암컷", "성체"]} transaction_image={[TmpTurtle1]} progress="거래가능"/>
+          <TransactionTurtle scientific_name="레이저 백 거북" price={94000000} transaction_tag={["수컷", "베이비"]} transaction_image={[TmpTurtle2]} progress="거래가능"/>
+          <TransactionTurtle scientific_name="아프리카 사이드 넥" price={110000000} transaction_tag={["암컷", "베이비"]} transaction_image={[TmpTurtle3]} progress="거래가능"/>
+          <TransactionTurtle scientific_name="미시시피 지도 거북이" price={700000000} transaction_tag={["암컷", "아성체"]} transaction_image={[TmpTurtle4]} progress="거래가능"/>
+          <TransactionTurtle scientific_name="지오프리 사이드 넥 터틀" price={67200000} transaction_tag={["암컷", "성체"]} transaction_image={[TmpTurtle5]} progress="거래가능"/>
+          <TransactionTurtle scientific_name="암보니아 상자 거북" price={43000000} transaction_tag={["수컷", "성체"]} transaction_image={[TmpTurtle6]} progress="거래가능"/>
         </div>
       </div>
 
