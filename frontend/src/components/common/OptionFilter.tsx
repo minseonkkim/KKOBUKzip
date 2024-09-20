0b1907f0999e8cf1c12cad64f3b7f06d6726c54b
minseonkkim <minsun9856@gmail.com>
2024-09-20 16:35:15 +0900

    feat: 최소 체중/가격이 최대 체중/가격보다 클 때 경고 알림창

--- frontend/src/components/common/OptionFilter.tsx
+++ frontend/src/components/common/OptionFilter.tsx
@@ -1,9 +1,5 @@
-// 거래, 경매의 옵션 필터
-// custom hook 의 useTradeFilter.tsx와 종속성이 있음, 수정 시 같이 수정 필요
-
 import { ChangeEvent } from "react";
 
-// 뭔가 빠진듯한 옵션 -> 경매 시작 전/ 경매중/유찰/낙찰 필터?
 type FilterType = "gender" | "minWeight" | "maxWeight" | "minPrice" | "maxPrice";
 
 function OptionFilter({
@@ -14,7 +10,7 @@ function OptionFilter({
   filters: { gender: string; minWeight: string; maxWeight: string; minPrice: string; maxPrice: string };
   updateFilter: (filterType: FilterType, value: string) => void;
   filterApplyHandle: () => void;
-})  {
+}) {
   const formatNumberWithCommas = (value: string): string => {
     const numberValue = value.replace(/,/g, '');
     if (!isNaN(Number(numberValue)) && numberValue !== '') {
@@ -22,9 +18,28 @@ function OptionFilter({
     }
     return '';
   };
-  
+
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.target.value = formatNumberWithCommas(e.target.value);
+  };
+
+  const handleFilterApply = () => {
+    const minWeight = Number(filters.minWeight);
+    const maxWeight = Number(filters.maxWeight);
+    const minPrice = Number(filters.minPrice.replace(/,/g, ''));
+    const maxPrice = Number(filters.maxPrice.replace(/,/g, ''));
+
+    if (minWeight > maxWeight) {
+      alert("최소 체중은 최대 체중보다 클 수 없습니다.");
+      return;
+    }
+
+    if (minPrice > maxPrice) {
+      alert("최소 가격은 최대 가격보다 클 수 없습니다.");
+      return;
+    }
+
+    filterApplyHandle();
   };
 
   return (
@@ -69,45 +84,40 @@ function OptionFilter({
           </div>
         </div>
         <div className="mb-4 flex flex-row items-center">
-          <label className="block mb-2 font-bold text-lg w-[100px]">
-              체중
-            </label>
-            <div className="flex space-x-4 items-center">
-              <div>
-                <input
-                  value={filters.minWeight}
-                  onChange={(e) => updateFilter("minWeight", e.target.value)}
-                  className="mr-1 w-[180px] h-[38px] bg-[#f2f2f2] focus:outline-none rounded-[10px] p-1"
-                  placeholder="최소 체중"
-                  onInput={(e) => {
-                    const target = e.target as HTMLInputElement;
-                    target.value = target.value.replace(/[^0-9]/g, '');
-                  }}
-                />
-                <span>kg</span>
-              </div>
-              <span className="text-[22px]">~</span>
-              <div>
-                <input
-                  value={filters.maxWeight}
-                  
-                  onChange={(e) => updateFilter("maxWeight", e.target.value)}
-                  className="mr-1 w-[180px] h-[38px] bg-[#f2f2f2] focus:outline-none rounded-[10px] p-1"
-                  placeholder="최대 체중"
-                  onInput={(e) => {
-                    const target = e.target as HTMLInputElement;
-                    target.value = target.value.replace(/[^0-9]/g, '');
-                  }}
-                />
-                <span>kg</span>
-              </div>
+          <label className="block mb-2 font-bold text-lg w-[100px]">체중</label>
+          <div className="flex space-x-4 items-center">
+            <div>
+              <input
+                value={filters.minWeight}
+                onChange={(e) => updateFilter("minWeight", e.target.value)}
+                className="mr-1 w-[180px] h-[38px] bg-[#f2f2f2] focus:outline-none rounded-[10px] p-1"
+                placeholder="최소 체중"
+                onInput={(e) => {
+                  const target = e.target as HTMLInputElement;
+                  target.value = target.value.replace(/[^0-9]/g, '');
+                }}
+              />
+              <span>kg</span>
             </div>
+            <span className="text-[22px]">~</span>
+            <div>
+              <input
+                value={filters.maxWeight}
+                onChange={(e) => updateFilter("maxWeight", e.target.value)}
+                className="mr-1 w-[180px] h-[38px] bg-[#f2f2f2] focus:outline-none rounded-[10px] p-1"
+                placeholder="최대 체중"
+                onInput={(e) => {
+                  const target = e.target as HTMLInputElement;
+                  target.value = target.value.replace(/[^0-9]/g, '');
+                }}
+              />
+              <span>kg</span>
+            </div>
+          </div>
         </div>
         <div className="flex flex-row justify-between">
           <div className="flex flex-row items-center">
-            <label className="block mb-2 font-bold text-lg w-[100px]">
-              가격
-            </label>
+            <label className="block mb-2 font-bold text-lg w-[100px]">가격</label>
             <div className="flex space-x-4 items-center">
               <input
                 value={filters.minPrice}
@@ -128,7 +138,7 @@ function OptionFilter({
           </div>
           <button
             type="button"
-            onClick={filterApplyHandle}
+            onClick={handleFilterApply}
             className="bg-[#4B721F] rounded-[5px] px-3 py-1 text-white font-bold"
           >
             검색
