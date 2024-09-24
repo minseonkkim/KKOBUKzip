fb75dab80ca12e24f97949f5db78bcd5c63254fc
minseonkkim <minsun9856@gmail.com>
2024-09-24 12:00:29 +0900

    feat: 반응형 메인 화면 구현

--- frontend/src/pages/common/MainPage.tsx
+++ frontend/src/pages/common/MainPage.tsx
@@ -13,26 +13,32 @@ function MainPage() {
   const [showTurtleMoving, setShowTurtleMoving] = useState(true);
   const [showContent, setShowContent] = useState(false);
   const [showButtons, setShowButtons] = useState(false);
+  const [showTurtle, setShowTurtle] = useState(true);
 
   useEffect(() => {
-    // 거북이 애니메이션과 텍스트 애니메이션을 동시에 시작
+    // Check window height to decide if the turtle should be shown
+    const handleResize = () => {
+      setShowTurtle(window.innerHeight > 730);
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener("resize", handleResize);
+
     const turtleTimer = setTimeout(() => {
       setShowTurtleMoving(false);
     }, 2300);
 
-    // 텍스트가 깜빡이기 시작하는 시간 조정 (2300ms에 동시 시작)
     const contentTimer = setTimeout(() => {
       setShowContent(true);
     }, 0);
 
-    let contentCloseTimer: NodeJS.Timeout;
+    let contentCloseTimer:NodeJS.Timeout;
     if (isMobile) {
       contentCloseTimer = setTimeout(() => {
         setShowContent(false);
       }, 2000);
     }
 
-    // 버튼이 나타나는 타이밍 조정 (텍스트가 깜빡인 후 2초 뒤)
     const buttonTimer = setTimeout(() => {
       setShowButtons(true);
     }, 2000);
@@ -44,6 +50,7 @@ function MainPage() {
         clearTimeout(contentCloseTimer);
       }
       clearTimeout(buttonTimer);
+      window.removeEventListener("resize", handleResize);
     };
   }, [isMobile]);
 
@@ -53,122 +60,157 @@ function MainPage() {
         <title>꼬북ZIP</title>
       </Helmet>
       <Header />
-      <div className="w-full h-full">
-      <img src={BackgroundImg} className="object-cover min-h-screen" draggable="false" />
+      <div className="relative w-full h-full min-h-screen bg-[#363636] overflow-hidden">
+        <img
+          src={BackgroundImg}
+          className="absolute inset-0 w-full h-full object-cover object-center"
+          alt="Background"
+          draggable="false"
+        />
 
-      {showTurtleMoving ? <img src={TurtleStop} className="w-[380px] absolute" style={{ bottom: "15px", right: "200px" }} draggable="false" /> : <img src={TurtleMoving} className="w-[380px] absolute turtle-animation" draggable="false" />}
-
-      {showContent && (
-        <div className="absolute top-[160px] left-0 w-full text-center flex flex-col items-center">
-          {isMobile ? (
-            <div>
-              <p className="font-dnf-bitbit text-[35px] text-white blinking-text">당신만의 거북이를</p>
-              <p className="font-dnf-bitbit text-[35px] text-white blinking-text">찾아보세요!</p>
-            </div>
+        {showTurtle && (
+          showTurtleMoving ? (
+            <img
+              src={TurtleStop}
+              className="w-[270px] md:w-[300px] lg:w-[380px] absolute"
+              style={{ bottom: "15px", right: "10%" }}
+              draggable="false"
+              alt="Turtle Stop"
+            />
           ) : (
-            <span className="font-dnf-bitbit text-[46px] text-white blinking-text">당신만의 거북이를 찾아보세요!</span>
+            <img
+              src={TurtleMoving}
+              className="w-[270px] md:w-[300px] lg:w-[380px] absolute turtle-animation"
+              draggable="false"
+              alt="Turtle Moving"
+            />
+          )
+        )}
+        <div className="absolute top-[170px] w-full text-center flex flex-col items-center">
+          {showContent && (
+            <div className="left-0 text-center flex flex-col items-center">
+              {isMobile ? (
+                <div>
+                  <p className="font-dnf-bitbit text-[30px] md:text-[35px] text-white blinking-text">
+                    당신만의 거북이를
+                  </p>
+                  <p className="font-dnf-bitbit text-[30px] md:text-[35px] text-white blinking-text">
+                    찾아보세요!
+                  </p>
+                </div>
+              ) : (
+                <span className="font-dnf-bitbit text-[36px] md:text-[40px] xl:text-[46px] text-white blinking-text px-[90px]">
+                  당신만의 거북이를 찾아보세요!
+                </span>
+              )}
+            </div>
+          )}
+
+          {showButtons && (
+            <div
+              className={`mt-5 text-center flex flex-col items-center ${
+                isMobile ? "gap-4" : "gap-[1.5rem]"
+              }`}
+            >
+              <div className="flex flex-col md:flex-row gap-4">
+                <Link to="/transaction-list">
+                  <div className="bg-[#dfdfdf] shadow-[3px_3px_0px_#858585] rounded-[10px] px-4 py-2 flex flex-row items-center cursor-pointer font-dnf-bitbit active:scale-95">
+                    <span className="whitespace-nowrap text-gray-500 text-[21px] md:text-[25px]">
+                      판매중인 거북이 →
+                    </span>
+                  </div>
+                </Link>
+                <Link to="/auction-list">
+                  <div className="bg-[#dfdfdf] shadow-[3px_3px_0px_#858585] rounded-[10px] px-4 py-2 flex flex-row items-center cursor-pointer font-dnf-bitbit active:scale-95">
+                    <span className="whitespace-nowrap text-gray-500 text-[21px] md:text-[25px]">
+                      경매중인 거북이 →
+                    </span>
+                  </div>
+                </Link>
+              </div>
+            </div>
           )}
         </div>
-      )}
-
-      {showButtons && (
-        <div className={ isMobile ? "absolute top-[calc(50vh-60px)] w-full text-center flex flex-col items-center" : "absolute top-[230px] left-0 w-full text-center flex flex-col items-center mt-5"}>
-          <div className={ isMobile ? "flex flex-col gap-4" : "flex flex-row gap-[1.5rem]"}>
-            <Link to="/transaction-list">
-              <div className="bg-[#dfdfdf] shadow-[3px_3px_0px_#858585] rounded-[10px] px-4 py-2 flex flex-row items-center cursor-pointer font-dnf-bitbit active:scale-95">
-                <span className="text-gray-500 text-[25px]">판매중인 거북이 →</span>
-              </div>
-            </Link>
-            <Link to="/auction-list">
-              <div className="bg-[#dfdfdf] shadow-[3px_3px_0px_#858585] rounded-[10px] px-4 py-2 flex flex-row items-center cursor-pointer font-dnf-bitbit active:scale-95">
-                <span className="text-gray-500 text-[25px]">경매중인 거북이 →</span>
-              </div>
-            </Link>
-          </div>
-        </div>
-        
-      )}
       </div>
       <style>{`
-  .turtle-animation {
-    animation: moveTurtle 3s forwards ease-in-out;
-  }
+        .turtle-animation {
+          animation: moveTurtle 3s forwards ease-in-out;
+        }
 
-  @keyframes moveTurtle {
-    0% {
-      bottom: 4.5%;
-      right: 18.5%;
-      transform: scale(0.97) rotate(0deg); 
-    }
-    10% {
-      bottom: 6.5%;
-      right: 21%;
-      transform: scale(0.94) rotate(5deg);
-    }
-    20% {
-      bottom: 6.5%;
-      right: 21%;
-      transform: scale(0.94) rotate(-5deg);
-    }
-    30% {
-      bottom: 8%;
-      right: 27%;
-      transform: scale(0.9) rotate(3deg);
-    }
-    40% {
-      bottom: 8%;
-      right: 27%;
-      transform: scale(0.9) rotate(-3deg);
-    }
-    50% {
-      bottom: 10%;
-      right: 33%;
-      transform: scale(0.85) rotate(2deg);
-    }
-    60% {
-      bottom: 10%;
-      right: 33%;
-      transform: scale(0.85) rotate(-2deg);
-    }
-    70% {
-      bottom: 12%;
-      right: 37%;
-      transform: scale(0.78) rotate(1deg);
-    }
-    80% {
-      bottom: 12%;
-      right: 37%;
-      transform: scale(0.78) rotate(-1deg);
-    }
-    90% {
-      bottom: 13%;
-      right: 38%;
-      transform: scale(0.7) rotate(0deg);
-    }
-    100% {
-      bottom: 13.5%;
-      right: 40%;
-      transform: scale(0.67) translate(0%, 0%) rotate(0deg);
-    }
-  }
+        @keyframes moveTurtle {
+          0% {
+            bottom: 4.5%;
+            right: 18.5%;
+            transform: scale(0.97) rotate(0deg); 
+          }
+          10% {
+            bottom: 6.5%;
+            right: 21%;
+            transform: scale(0.94) rotate(5deg);
+          }
+          20% {
+            bottom: 6.5%;
+            right: 21%;
+            transform: scale(0.94) rotate(-5deg);
+          }
+          30% {
+            bottom: 8%;
+            right: 27%;
+            transform: scale(0.9) rotate(3deg);
+          }
+          40% {
+            bottom: 8%;
+            right: 27%;
+            transform: scale(0.9) rotate(-3deg);
+          }
+          50% {
+            bottom: 10%;
+            right: 33%;
+            transform: scale(0.85) rotate(2deg);
+          }
+          60% {
+            bottom: 10%;
+            right: 33%;
+            transform: scale(0.85) rotate(-2deg);
+          }
+          70% {
+            bottom: 11.7%;
+            right: 37%;
+            transform: scale(0.78) rotate(1deg);
+          }
+          80% {
+            bottom: 11.7%;
+            right: 37%;
+            transform: scale(0.78) rotate(-1deg);
+          }
+          90% {
+            bottom: 12.3%;
+            right: 38%;
+            transform: scale(0.7) rotate(0deg);
+          }
+          100% {
+            bottom: 13%;
+            right: 39.3%;
+            transform: scale(0.67) translate(0%, 0%) rotate(0deg);
+          }
+        }
 
+        .blinking-text {
+          animation: blink 1s 2;
+        }
 
-  .blinking-text {
-    animation: blink 1s 2;
-  }
-
-  @keyframes blink {
-    0% {
-      opacity: 1;
-    }
-    50% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-`}</style>
+        @keyframes blink {
+          0% {
+            opacity: 1;
+          }
+          50% {
+            opacity: 0;
+          }
+          100% {
+            opacity: 1;
+          }
+        }
+      `}</style>
     </>
   );
 }
