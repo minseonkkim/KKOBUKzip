ac9f17fbf2f3d127b56c57ee5c1ac1500a64d403
신민경 <tlsalsrud252@naver.com>
2024-09-24 10:36:16 +0900

    fix: resolve build error

--- backend/main-service/src/main/java/com/turtlecoin/mainservice/domain/user/service/CustomUserDetailsService.java
+++ backend/main-service/src/main/java/com/turtlecoin/mainservice/domain/user/service/CustomUserDetailsService.java
@@ -1,27 +1,27 @@
-package com.turtlecoin.mainservice.domain.user.service;
-
-import com.turtlecoin.mainservice.domain.user.dto.CustomUserDetails;
-import com.turtlecoin.mainservice.domain.user.entity.User;
-import com.turtlecoin.mainservice.domain.user.repository.UserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-    private final UserRepository userRepository;
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User userData = userRepository.findByname(username);
-        if(userData!=null){
-            return new CustomUserDetails(userData);
-        }
-        return null;
-    }
-}
+//package com.turtlecoin.mainservice.domain.user.service;
+//
+//import com.turtlecoin.mainservice.domain.user.dto.CustomUserDetails;
+//import com.turtlecoin.mainservice.domain.user.entity.User;
+//import com.turtlecoin.mainservice.domain.user.repository.UserRepository;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
+//    private final UserRepository userRepository;
+//    public CustomUserDetailsService(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User userData = userRepository.findByname(username);
+//        if(userData!=null){
+//            return new CustomUserDetails(userData);
+//        }
+//        return null;
+//    }
+//}
