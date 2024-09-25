0e3701ec03fe86d5e1543f67fff09f927d50c3b2
서규범 <seo980916@gmail.com>
2024-09-25 17:17:34 +0900

    fix: 컨트랙트 truffle 배포 코드 수정

--- blockchain/migrations/3_deploy_TurtleEscrow.js
+++ blockchain/migrations/3_deploy_TurtleEscrow.js
@@ -1,15 +1,12 @@
-const TurtleToken = artifacts.require("TurtleToken");
+const TurtleEscrow = artifacts.require("TurtleEscrow");
+require('dotenv').config()
 
 module.exports = async function (deployer) {
-  // web3.utils.toWei를 사용하여 큰 숫자 처리
-  const initialSupply = web3.utils.toWei("1000000000", "ether"); // 1,000,000,000 TURT (18자리 소수점 사용)
-  const exchangeRate = web3.utils.toWei("5000000", "ether"); // 5,000,000 TURT / 1 ETH
-
-  // TurtleToken 배포
-  await deployer.deploy(TurtleToken, initialSupply, exchangeRate);
-  const turtleTokenInstance = await TurtleToken.deployed();
+  // TurtleEscrow 배포
+  await deployer.deploy(process.env.TURTLE_TOKEN_ADDRESS);
+  const turtleEscrowInstance = await TurtleEscrow.deployed();
 
   console.log("===========================================================");
   console.log("===========================================================");
-  console.log("TurtleToken deployed at address:", turtleTokenInstance.address);
+  console.log("TurtleEscrow deployed at address:", turtleEscrowInstance.address);
 };
