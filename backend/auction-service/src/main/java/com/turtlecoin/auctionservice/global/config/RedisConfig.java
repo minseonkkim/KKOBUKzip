19e3089b89a46e1362615c2cafaf5e89f0d5d5fc
김영빈 <kyb031700@naver.com>
2024-09-27 10:55:16 +0900

    Fix : API 주소 수정

--- backend/auction-service/src/main/java/com/turtlecoin/auctionservice/global/config/RedisConfig.java
+++ backend/auction-service/src/main/java/com/turtlecoin/auctionservice/global/config/RedisConfig.java
@@ -11,39 +11,26 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
     @Value("${spring.data.redis.host}")
     private String host;
+
     @Value("${spring.data.redis.port}")
     private int port;
+
     @Value("${spring.data.redis.password}")
     private String password;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
-        redisConfiguration.setHostName(host);
-        redisConfiguration.setPort(port);
-        redisConfiguration.setPassword(password);
-        return new LettuceConnectionFactory(redisConfiguration);
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        // RedisStandaloneConfiguration에 비밀번호 설정
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(host);
+        redisConfig.setPort(port);
+        redisConfig.setPassword(password);
+
+        return new LettuceConnectionFactory(redisConfig);
     }
-    // @Bean
-    // public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-    //     RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-    //     redisTemplate.setConnectionFactory(connectionFactory);
-    //
-    //     // Key와 Value의 직렬화 방식 설정
-    //     redisTemplate.setKeySerializer(new StringRedisSerializer());
-    //     redisTemplate.setValueSerializer(new StringRedisSerializer());
-    //     redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-    //     redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-    //
-    //     return redisTemplate;
-    // }
-    //
-    // @Bean
-    // public LettuceConnectionFactory lettuceConnectionFactory() {
-    //     return new LettuceConnectionFactory();
-    // }
 
     @Bean
     RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -52,3 +39,4 @@ public class RedisConfig {
         return template;
     }
 }
+
