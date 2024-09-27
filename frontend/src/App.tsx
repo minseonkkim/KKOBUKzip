bd342455147cee2df331060eebb763ff8f848db1
minseonkkim <minsun9856@gmail.com>
2024-09-27 15:40:54 +0900

    fix: 로딩 이미지 크기, 로딩 시 배경색 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -65,7 +65,7 @@ function App() {
             <div className="w-full h-[100vh] flex flex-col justify-center items-center">
               <img
                 src={LoadingImage}
-                className="loading-image"
+                className="loading-image w-[500px] h-auto"
                 alt="Loading"
                 draggable="false"
               />
