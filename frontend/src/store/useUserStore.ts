76d5a67c50cb75a83429c4f753dd8ffd978ace53
한세훈 <atdark04@gmail.com>
2024-09-30 10:15:48 +0900

    fix: 거북이 조회 api 명세에 맞게 수정

--- frontend/src/store/useUserStore.ts
+++ frontend/src/store/useUserStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-// import { createJSONStorage, persist } from "zustand/middleware";
+import { createJSONStorage, persist } from "zustand/middleware";
 import { UserInfo } from "../types/user";
 
 interface UserStore {
@@ -11,24 +11,36 @@ interface UserStore {
 }
 
 export const useUserStore = create<UserStore>()(
-  // persist(
-  (set) => ({
-    userInfo: null,
-    isLogin: false,
+  persist(
+    (set) => ({
+      userInfo: null,
+      isLogin: false,
 
-    setLogin: (userInfo: UserInfo) => set({ userInfo, isLogin: true }),
-    setLogout: () => set({ userInfo: null, isLogin: false }),
-    setIsLogin: (isLogin: boolean) => {
-      return set({ isLogin });
-    },
-  })
-  // {
-  //   name: "userStore",
-  //   storage: createJSONStorage(() => localStorage),
-  //   partialize: (state) => ({
-  //     userInfo: state.userInfo,
-  //     isLogin: state.isLogin,
-  //   }),
-  // }
-  // )
+      setLogin: (userInfo: UserInfo) =>
+        set({
+          userInfo: {
+            userId: userInfo.userId,
+            email: userInfo.email,
+            address: userInfo.address,
+            phoneNumber: userInfo.phoneNumber,
+            nickname: userInfo.nickname,
+            profileImage: userInfo.profileImage,
+            role: userInfo.role,
+          },
+          isLogin: true,
+        }),
+      setLogout: () => set({ userInfo: null, isLogin: false }),
+      setIsLogin: (isLogin: boolean) => {
+        return set({ isLogin });
+      },
+    }),
+    {
+      name: "userStore",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({
+        userInfo: state.userInfo,
+        isLogin: state.isLogin,
+      }),
+    }
+  )
 );
