a69a035fffec11b8c5a4f3abfcd2a4f73bd40954
YBK1 <kyb031700@naver.com>
2024-10-01 16:25:08 +0900

    Feat : 동적 스케줄링 추가

---
+++ backend/auction-service/src/main/java/com/turtlecoin/auctionservice/domain/global/internal/SchedulingTask.java
@@ -0,0 +1,25 @@
+package com.turtlecoin.auctionservice.domain.global.internal;
+
+import lombok.AllArgsConstructor;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.function.Consumer;
+
+@AllArgsConstructor
+public class SchedulingTask<T> implements Runnable {
+    // target은 입력값 역할을 함
+    // 우리 프로젝트에서는 auctionID를 받아오는 것이 될듯
+    private final T target;
+    // 실제 수행할 함수
+    private final Consumer<T> task;
+
+    private final TransactionTemplate transactionTemplate;
+
+    @Override
+    public void run() {
+        transactionTemplate.execute(status -> {
+            task.accept(target);
+            return null;
+        });
+    }
+}
---
+++ backend/auction-service/src/main/java/com/turtlecoin/auctionservice/global/config/SchedulerConfig.java
@@ -0,0 +1,28 @@
+package com.turtlecoin.auctionservice.global.config;
+
+import com.turtlecoin.auctionservice.domain.auction.entity.Auction;
+import com.turtlecoin.auctionservice.domain.auction.entity.AuctionProgress;
+import com.turtlecoin.auctionservice.domain.auction.repository.AuctionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Configuration
+public class SchedulerConfig {
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("scheduler-");
+        scheduler.setAwaitTerminationSeconds(60);
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        scheduler.initialize();
+        return scheduler;
+    }
+}
