51b5d67c24772dfde54abb127f06f0440ce77b0c
서규범 <seo980916@gmail.com>
2024-10-02 13:07:34 +0900

    feat: 양도서류작성 페이지 가이드 기능 추가

--- frontend/src/components/document/GrantorDocument.tsx
+++ frontend/src/components/document/GrantorDocument.tsx
@@ -7,6 +7,7 @@ import {
   GrantorFetchData,
 } from "../../types/document";
 import { createGrantDocumentRequest } from "../../apis/documentApis";
+import { grantDoc } from "../../utils/grantDriverObject";
 
 interface ApplicantInfoContext {
   applicantName: string,
@@ -90,187 +91,195 @@ function GrantorDocument() {
     setGrantor((prev) => ({ ...prev, [key]: evt.target.value }));
   };
 
+  const handleGuide = () => {
+    grantDoc.drive();
+  }
+
   return (
     <>
       <Helmet>
         <title>양도서류작성</title>
       </Helmet>
 
-      <div className="mb-8">
-        <div className="w-full flex mb-4">
-          <span className="text-xl font-semibold flex-1">양도인</span>
-          <label
-            htmlFor="loadUserData"
-            className="cursor-pointer select-none mr-4 flex items-center gap-2"
-          >
-            <span>신청인 정보 불러오기</span>
-            <input onClick={loadUserData} type="checkbox" id="loadUserData" />
-          </label>
-        </div>
+      <button onClick={handleGuide} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">가이드 시작</button>
 
-        {/* 양도인 정보 */}
-        <div className="space-y-2">
-          <div className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-4">
-            <div className="flex items-center">
-              <span className="w-1/3 font-medium">성명(상호)</span>
-              <input
-                type="text"
-                placeholder="성명(상호)"
-                value={grantor.name}
-                onChange={(evt) => changeHandle("name", evt)}
-                className="w-2/3 px-3 py-2 border rounded"
-              />
-            </div>
-
-            <div className="flex items-center">
-              <span className="w-1/3 font-medium">전화번호</span>
-              <input
-                type="text"
-                placeholder="전화번호"
-                value={grantor.phoneNumber}
-                onChange={(evt) => changeHandle("phoneNumber", evt)}
-                className="w-2/3 px-3 py-2 border rounded [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-              />
-            </div>
-          </div>
-          <div className="flex items-center">
-            <label className="w-[31%] md:w-1/5 font-medium break-keep">
-              인공증식 시설 소재지
-            </label>
-
-            <button
-              ref={addressBtnRef}
-              className="hidden md:inline w-1/12 md:w-1/12 hover:bg-gray-100 border py-2 ml-1.5 rounded"
-              onClick={loadPostcodeSearch}
+      <div id="container">
+        <div className="mb-8">
+          <div className="w-full flex mb-4">
+            <span className="text-xl font-semibold flex-1">양도인</span>
+            <label
+              htmlFor="loadUserData"
+              className="cursor-pointer select-none mr-4 flex items-center gap-2"
             >
-              찾기
-            </button>
-            <div className="flex flex-col w-[67%] md:flex-row md:w-full space-y-1 md:space-y-0">
-              <input
-                type="text"
-                className="w-full px-3 ml-2 py-2 border rounded cursor-pointer hover:bg-gray-100"
-                placeholder="기본주소"
-                readOnly
-                // onChange={(evt) => changeHandle("address", evt)}
-                value={postcodeData?.roadAddress || ""}
-                onClick={() => addressBtnRef.current?.click()}
-              />
-              <input
-                type="text"
-                onChange={(evt) => {
-                  setDetailLocation(evt.target.value);
-                }}
-                className="w-full px-3 py-2 border rounded ml-2"
-                placeholder="상세주소"
-              />
-            </div>
-          </div>
-        </div>
-        {/* 양도인 정보 끝 */}
-      </div>
-
-      {/* 양수인 정보 - 불러올 것 */}
-      <div className="mb-8">
-        <h3 className="text-xl font-semibold mb-4">양수인</h3>
-        <div className="grid grid-cols-1 gap-y-4 md:grid-cols-2 md:gap-x-8 md:gap-y-2">
-          <div className="flex">
-            <span className="w-1/3 font-medium">성명</span>
-            <span className="w-2/3">사전에 설정된 양수인 정보 불러올 것</span>
-          </div>
-          <div className="flex">
-            <span className="w-1/3 font-medium">전화번호</span>
-            <span className="w-2/3">010-3333-3333</span>
-          </div>
-
-          <div className="flex">
-            <span className="w-1/3 font-medium">주소</span>
-            <span className="w-2/3">광주광역시 하남산단6번로 107</span>
-          </div>
-        </div>
-      </div>
-      {/* 양수인 정보 끝 */}
-
-      {/* 개체 정보 - 사전에 설정된 정보 불러올 것 */}
-      <div className="mb-8">
-        <h3 className="text-xl font-semibold mb-4">개체 정보 작성</h3>
-        <div className="grid grid-cols-1 gap-y-1 md:grid-cols-2 md:gap-x-8 md:gap-y-2">
-          <div className="flex">
-            <span className="w-1/3 font-medium">학명</span>
-            <span className="w-2/3 px-3 py-2">Malaclemys terrapin</span>
-          </div>
-          <div className="flex items-center">
-            <span className="w-1/3 font-medium">보통명(일반명)</span>
-            <span className="w-2/3 px-3 py-2">(공란)</span>
-          </div>
-          <div className="flex items-center">
-            <span className="w-1/3 font-medium break-keep">부속서 등급</span>
-            <span className="w-2/3 px-3 py-2">II급</span>
-          </div>
-          <div className="flex items-center">
-            <label className="w-1/3 font-medium">수량</label>
-            <span className="w-2/3 px-3 py-2">1</span>
-          </div>
-          <div className="flex items-center">
-            <span className="w-1/3 font-medium">형태</span>
-            <span className="w-2/3 px-3 py-2">살아있는 생물</span>
-          </div>
-          <div className="flex items-center">
-            <label className="w-1/3 font-medium">용도</label>
-            <span className="w-2/3 px-3 py-2">양수인 입력 값</span>
-          </div>
-          <div className="flex items-center">
-            <span className="w-1/3 font-medium">양수사유</span>
-            <span className="w-2/3 px-3 py-2">양수인 입력 값</span>
-          </div>
-          <div className="flex items-center">
-            <label className="w-1/3 font-medium">개체식별번호</label>
-            <span className="w-2/3 px-3 py-2">양수인 입력 값</span>
-          </div>
-        </div>
-      </div>
-      {/* 개체 정보 끝 */}
-
-      {/* 구비서류 정보 */}
-      <div className="mb-8">
-        <h3 className="text-xl font-semibold mb-4">구비서류</h3>
-        <div className="space-y-4">
-          <div>
-            <label className="block font-semibold mb-1 break-keep">
-              수입허가증 등 양도하려는 국제적 멸종위기종의 입수 경위 및 이를
-              증명하는 서류
+              <span>신청인 정보 불러오기</span>
+              <input onClick={loadUserData} type="checkbox" id="loadUserData" />
             </label>
-            <div className="w-full px-3 py-2 border rounded bg-gray-50 flex items-center">
-              <span className="text-gray-500 flex-grow">서류 번호</span>
-              <input type="file" className="hidden" id="file1" />
-              <label htmlFor="file1" className="cursor-pointer flex-shrink">
-                서류 찾기
-              </label>
-            </div>
           </div>
 
-          <div>
-            <label className="block font-semibold mb-1 break-keep">
-              양도한 국제적 멸종위기종의 부모 개체의 고유번호
-            </label>
-            <div className="w-full px-3 py-2 border rounded bg-gray-50 flex items-center mb-2">
-              <span className="text-gray-500 flex-grow">부 개체 번호</span>
-              <input type="file" className="hidden" id="file1" />
-              <label htmlFor="file1" className="cursor-pointer flex-shrink">
-                개체 찾기
+          {/* 양도인 정보 */}
+          <div className="space-y-2">
+            <div className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-4">
+              <div className="flex items-center">
+                <span className="w-1/3 font-medium">성명(상호)</span>
+                <input
+                  type="text"
+                  placeholder="성명(상호)"
+                  value={grantor.name}
+                  onChange={(evt) => changeHandle("name", evt)}
+                  className="w-2/3 px-3 py-2 border rounded"
+                />
+              </div>
+
+              <div className="flex items-center">
+                <span className="w-1/3 font-medium">전화번호</span>
+                <input
+                  type="text"
+                  placeholder="전화번호"
+                  value={grantor.phoneNumber}
+                  onChange={(evt) => changeHandle("phoneNumber", evt)}
+                  className="w-2/3 px-3 py-2 border rounded [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                />
+              </div>
+            </div>
+            <div className="flex items-center">
+              <label className="w-[31%] md:w-1/5 font-medium break-keep">
+                인공증식 시설 소재지
               </label>
+
+              <button
+                ref={addressBtnRef}
+                className="hidden md:inline w-1/12 md:w-1/12 hover:bg-gray-100 border py-2 ml-1.5 rounded"
+                onClick={loadPostcodeSearch}
+              >
+                찾기
+              </button>
+              <div className="flex flex-col w-[67%] md:flex-row md:w-full space-y-1 md:space-y-0">
+                <input
+                  type="text"
+                  className="w-full px-3 ml-2 py-2 border rounded cursor-pointer hover:bg-gray-100"
+                  placeholder="기본주소"
+                  readOnly
+                  // onChange={(evt) => changeHandle("address", evt)}
+                  value={postcodeData?.roadAddress || ""}
+                  onClick={() => addressBtnRef.current?.click()}
+                />
+                <input
+                  type="text"
+                  onChange={(evt) => {
+                    setDetailLocation(evt.target.value);
+                  }}
+                  className="w-full px-3 py-2 border rounded ml-2"
+                  placeholder="상세주소"
+                />
+              </div>
+            </div>
+          </div>
+          {/* 양도인 정보 끝 */}
+        </div>
+
+        {/* 양수인 정보 - 불러올 것 */}
+        <div className="mb-8">
+          <h3 className="text-xl font-semibold mb-4">양수인</h3>
+          <div className="grid grid-cols-1 gap-y-4 md:grid-cols-2 md:gap-x-8 md:gap-y-2">
+            <div className="flex">
+              <span className="w-1/3 font-medium">성명</span>
+              <span className="w-2/3">사전에 설정된 양수인 정보 불러올 것</span>
+            </div>
+            <div className="flex">
+              <span className="w-1/3 font-medium">전화번호</span>
+              <span className="w-2/3">010-3333-3333</span>
             </div>
 
-            <div className="w-full px-3 py-2 border rounded bg-gray-50 flex items-center">
-              <span className="text-gray-500 flex-grow">모 개체 번호</span>
-              <input type="file" className="hidden" id="file1" />
-              <label htmlFor="file1" className="cursor-pointer flex-shrink">
-                개체 찾기
-              </label>
+            <div className="flex">
+              <span className="w-1/3 font-medium">주소</span>
+              <span className="w-2/3">광주광역시 하남산단6번로 107</span>
             </div>
           </div>
         </div>
+        {/* 양수인 정보 끝 */}
+
+        {/* 개체 정보 - 사전에 설정된 정보 불러올 것 */}
+        <div className="mb-8">
+          <h3 className="text-xl font-semibold mb-4">개체 정보 작성</h3>
+          <div className="grid grid-cols-1 gap-y-1 md:grid-cols-2 md:gap-x-8 md:gap-y-2">
+            <div className="flex">
+              <span className="w-1/3 font-medium">학명</span>
+              <span className="w-2/3 px-3 py-2">Malaclemys terrapin</span>
+            </div>
+            <div className="flex items-center">
+              <span className="w-1/3 font-medium">보통명(일반명)</span>
+              <span className="w-2/3 px-3 py-2">(공란)</span>
+            </div>
+            <div className="flex items-center">
+              <span className="w-1/3 font-medium break-keep">부속서 등급</span>
+              <span className="w-2/3 px-3 py-2">II급</span>
+            </div>
+            <div className="flex items-center">
+              <label className="w-1/3 font-medium">수량</label>
+              <span className="w-2/3 px-3 py-2">1</span>
+            </div>
+            <div className="flex items-center">
+              <span className="w-1/3 font-medium">형태</span>
+              <span className="w-2/3 px-3 py-2">살아있는 생물</span>
+            </div>
+            <div className="flex items-center">
+              <label className="w-1/3 font-medium">용도</label>
+              <span className="w-2/3 px-3 py-2">양수인 입력 값</span>
+            </div>
+            <div className="flex items-center">
+              <span className="w-1/3 font-medium">양수사유</span>
+              <span className="w-2/3 px-3 py-2">양수인 입력 값</span>
+            </div>
+            <div className="flex items-center">
+              <label className="w-1/3 font-medium">개체식별번호</label>
+              <span className="w-2/3 px-3 py-2">양수인 입력 값</span>
+            </div>
+          </div>
+        </div>
+        {/* 개체 정보 끝 */}
+
+        {/* 구비서류 정보 */}
+        <div className="mb-8">
+          <h3 className="text-xl font-semibold mb-4">구비서류</h3>
+          <div className="space-y-4">
+            <div>
+              <label className="block font-semibold mb-1 break-keep">
+                수입허가증 등 양도하려는 국제적 멸종위기종의 입수 경위 및 이를
+                증명하는 서류
+              </label>
+              <div className="w-full px-3 py-2 border rounded bg-gray-50 flex items-center">
+                <span className="text-gray-500 flex-grow">서류 번호</span>
+                <input type="file" className="hidden" id="file1" />
+                <label htmlFor="file1" className="cursor-pointer flex-shrink">
+                  서류 찾기
+                </label>
+              </div>
+            </div>
+
+            <div>
+              <label className="block font-semibold mb-1 break-keep">
+                양도한 국제적 멸종위기종의 부모 개체의 고유번호
+              </label>
+              <div className="w-full px-3 py-2 border rounded bg-gray-50 flex items-center mb-2">
+                <span className="text-gray-500 flex-grow">부 개체 번호</span>
+                <input type="file" className="hidden" id="file1" />
+                <label htmlFor="file1" className="cursor-pointer flex-shrink">
+                  개체 찾기
+                </label>
+              </div>
+
+              <div className="w-full px-3 py-2 border rounded bg-gray-50 flex items-center">
+                <span className="text-gray-500 flex-grow">모 개체 번호</span>
+                <input type="file" className="hidden" id="file1" />
+                <label htmlFor="file1" className="cursor-pointer flex-shrink">
+                  개체 찾기
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* 구비서류 정보 끝 */}
       </div>
-      {/* 구비서류 정보 끝 */}
 
       <div className="flex justify-center space-x-4">
         <button
---
+++ frontend/src/utils/grantDriverObject.ts
@@ -0,0 +1,44 @@
+import { driver } from "driver.js";
+import "driver.js/dist/driver.css";
+
+export const grantDoc = driver({
+  showProgress: true,
+  steps: [
+    {
+      element: "#container > div:nth-child(1)",
+      popover: {
+        title: "양도인 정보 작성",
+        description: "개체를 판매하고자 하는 양도인의 정보를 입력해 주세요. '신청인 정보 불러오기' 버튼을 토글하여 현재 로그인한 신청인의 정보를 불러올 수 있습니다.",
+        side: "bottom",
+        align: "start"
+      }
+    },
+    {
+      element: "#container > div:nth-child(2)",
+      popover: {
+        title: "양수인 정보 확인",
+        description: "~~~~~~~~~~",
+        side: "bottom",
+        align: "start"
+      }
+    },
+    {
+      element: "#container > div:nth-child(3)",
+      popover: {
+        title: "개체 정보 확인",
+        description: "~~~~~~~~~~",
+        side: "bottom",
+        align: "start"
+      }
+    },
+    {
+      element: "#container > div:nth-child(4)",
+      popover: {
+        title: "구비 서류 작성",
+        description: "~~~~~~~~~~",
+        side: "bottom",
+        align: "start"
+      }
+    },
+  ]
+});
