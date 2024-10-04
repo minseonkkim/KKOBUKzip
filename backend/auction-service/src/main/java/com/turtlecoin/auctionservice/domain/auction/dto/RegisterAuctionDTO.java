905799a441a5a4c85c0b7b6d687f33604a1a0899
김여준 <kt65346337@gmail.com>
2024-10-04 16:44:38 +0900

    fix: 경매 등록 타입 변경

--- backend/auction-service/src/main/java/com/turtlecoin/auctionservice/domain/auction/dto/RegisterAuctionDTO.java
+++ backend/auction-service/src/main/java/com/turtlecoin/auctionservice/domain/auction/dto/RegisterAuctionDTO.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Getter
@@ -29,7 +30,7 @@ public class RegisterAuctionDTO {
     private int weight;   // 거북이 무게 추가
     private String sellerAddress;
     private Gender gender;   // 거북이 성별 추가
-    private List<AuctionTag> auctionTags;
+    private List<String> auctionTags;
 
     public Auction toEntity() {
         return Auction.builder()
@@ -42,7 +43,9 @@ public class RegisterAuctionDTO {
                 .nowBid(minBid)
                 .weight(weight)
                 .sellerAddress(sellerAddress)
-                .auctionTags(auctionTags != null ? auctionTags : new ArrayList<>())  // tags가 null일 경우 빈 리스트로 초기화
+                .auctionTags(auctionTags != null ? auctionTags.stream()
+                        .map(tag -> new AuctionTag(tag))  // String을 AuctionTag로 변환
+                        .collect(Collectors.toList()) : new ArrayList<>())  // tags가 null일 경우 빈 리스트로 초기화
                 .auctionProgress(AuctionProgress.BEFORE_AUCTION)
                 .endTime(startTime.plusSeconds(30))
                 .build();
