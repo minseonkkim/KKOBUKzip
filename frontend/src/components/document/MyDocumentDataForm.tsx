ab9cac05270166fc64807509c51871fd0b792235
서규범 <seo980916@gmail.com>
2024-10-06 00:01:43 +0900

    fix: 신청인 정보 중 주소 표현 수정

--- frontend/src/components/document/MyDocumentDataForm.tsx
+++ frontend/src/components/document/MyDocumentDataForm.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 function MyDocumentDataForm({ info }: Props) {
   // 신청인 정보 -데이터 연동되면 할당할 것
-
+  
   return (
     <div className="mb-8">
       <h3 className="text-xl font-semibold mb-4">신청인 정보</h3>
@@ -35,7 +35,7 @@ function MyDocumentDataForm({ info }: Props) {
         </div>
         <div className="flex items-center">
           <span className="w-1/3 font-medium">주소</span>
-          <span className="w-2/3 px-3 py-2">{info.address}</span>
+          <span className="w-2/3 px-3 py-2">{info.address + ' ' + info.detailedAddress}</span>
         </div>
       </div>
     </div>
