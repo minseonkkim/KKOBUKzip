f69f7a1b770e2d46327e75698512ee01d12e249d
YBK1 <kyb031700@naver.com>
2024-10-08 00:34:19 +0900

    Fix : 시간 구분선 오류 수정

--- frontend/src/store/useChatStore.ts
+++ frontend/src/store/useChatStore.ts
@@ -7,6 +7,7 @@ export interface ChattingStore {
   selectedChatTitle: null | string;
   openedFromTransaction: boolean;
   totalUnreadCount: number;
+  recentChattingTime: string | null;
 
   chatRoomList: ChatListItem[];
   toggleChat: () => void;
@@ -17,6 +18,7 @@ export interface ChattingStore {
   updateRoomList: (newChat: ChatListItem) => void;
   addTotalUnreadCount: (count: number) => void;
   subTotalUnreadCount: (count: number) => void;
+  setRecentChattingTime: (time: string) => void;
 }
 
 // 채팅 주소 구조 -> 상대 아이디, 내 아이디를 '-' 로 연결(작은쪽이 앞에)
@@ -27,6 +29,7 @@ const useChatStore = create<ChattingStore>((set) => ({
   openedFromTransaction: false,
   chatRoomList: [],
   totalUnreadCount: 0,
+  recentChattingTime: "blank",
 
   toggleChat: () =>
     set((state) => ({
@@ -153,6 +156,13 @@ const useChatStore = create<ChattingStore>((set) => ({
     set((state) => ({
       totalUnreadCount: state.totalUnreadCount - count,
     }));
+  },
+
+  setRecentChattingTime: (time: string) => {
+    set({
+      recentChattingTime: time,
+    });
+    console.log(time);
   },
 }));
 
