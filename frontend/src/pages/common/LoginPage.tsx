14c49aa0ca96459b212c2a5465c7b463fd181420
서규범 <seo980916@gmail.com>
2024-10-10 02:58:38 +0900

    fix: 로그인 실패 메시지 문구 오타 수정

--- frontend/src/pages/common/LoginPage.tsx
+++ frontend/src/pages/common/LoginPage.tsx
@@ -41,7 +41,7 @@ function LoginPage() {
       navigate("/");
     } else {
       console.log(error);
-      setFailMessage("이메일 또는 비밀번호를 올바르지 않습니다.");
+      setFailMessage("이메일 또는 비밀번호가 올바르지 않습니다.");
     }
   };
 
