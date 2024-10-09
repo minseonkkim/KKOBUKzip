be327548690b0ff8f9594be6c9479673f934db0f
김영빈 <kyb031700@naver.com>
2024-10-09 17:19:27 +0900

    Feat : AWS S3 CloudFront적용

--- backend/main-service/src/main/java/com/turtlecoin/mainservice/domain/s3/controller/S3Controller.java
+++ backend/main-service/src/main/java/com/turtlecoin/mainservice/domain/s3/controller/S3Controller.java
@@ -1,10 +1,15 @@
 package com.turtlecoin.mainservice.domain.s3.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,22 +22,31 @@ import lombok.RequiredArgsConstructor;
 public class S3Controller {
 	private final ImageUploadService imageUploadService;
 	// 이미지 업로드를 테스트하기 위한 컨트롤러 이므로 실제 사용하지 말것
-	@PostMapping(value = "/register/breed", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+	@PostMapping(value = "/upload/s3", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
 	public ResponseEntity<?> registerBreedingDocument(
 		@RequestPart("img1") MultipartFile img1,
 		@RequestPart("img2") MultipartFile img2,
 		@RequestPart("img3") MultipartFile img3) {
+		Map<String, String> result = new HashMap<>();
 
 		// S3에 이미지를 업로드
 		try{
-			imageUploadService.upload(img1, "test");
-			imageUploadService.upload(img2, "test");
-			imageUploadService.upload(img3, "test");
+			result.put("img1", imageUploadService.upload(img1, "test"));
+			result.put("img2", imageUploadService.upload(img2, "test"));
+			result.put("img3", imageUploadService.upload(img3, "test"));
 		}catch (Exception e){
 			//e.printStackTrace();
 		}
 
 		// 응답
-		return new ResponseEntity<>("Application with images received successfully!", HttpStatus.OK);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/delete/s3")
+	public ResponseEntity<?> deleteS3(@RequestBody Map<String, String> data){
+		String url = data.get("url");
+		imageUploadService.deleteS3(url);
+
+		return new ResponseEntity<>("Image deleted successfully!", HttpStatus.OK);
 	}
 }
