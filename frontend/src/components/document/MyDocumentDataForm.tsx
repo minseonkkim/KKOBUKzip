2979aec0ac291477a80bf1d22d835b0cf5aa82d1
서규범 <seo980916@gmail.com>
2024-10-10 01:47:09 +0900

    feat: 마이페이지 정보 표현 수정(address, documents, etc...)

--- frontend/src/components/document/MyDocumentDataForm.tsx
+++ frontend/src/components/document/MyDocumentDataForm.tsx
@@ -35,7 +35,7 @@ function MyDocumentDataForm({ info }: Props) {
         </div>
         <div className="flex items-center">
           <span className="w-1/3 font-medium">주소</span>
-          <span className="w-2/3 px-3 py-2">{info.address + ' ' + info.detailedAddress}</span>
+          <span className="w-2/3 px-3 py-2">{info.address.replace(" / ", " ")}</span>
         </div>
       </div>
     </div>
